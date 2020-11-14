c4c41267cb1b52a199aee3d21a6446225b150755
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-14 17:48:51 -0300

    chage to clientecontroller

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace DesafioGamificacaoCPFL.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public Cliente Get(string id)
+        {
+            return new Cliente
+            {
+                Cpf = "1234",
+                Nome = "ronaldo"
+            };
+        }
+    }
+}
---
+++ Models/Clientes.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesafioGamificacaoCPFL
+{
+    public class Cliente
+    {
+        public string Nome{ get; internal set; }
+
+        public string Cpf { get; internal set; }
+    }
+}
