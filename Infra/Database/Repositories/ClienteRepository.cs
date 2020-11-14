5ae48ad252889a3b2d6aa250de58fd8adaa77601
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-14 19:16:53 -0300

    create repositories

---
+++ Infra/Database/Repositories/ClienteRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DesafioGamificacaoCPFL.Infra.Database.Settings;
+
+namespace DesafioGamificacaoCPFL.Infra.Database.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly IMongoCollection<Cliente> _clientes;
+
+        public ClienteRepository(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.);
+
+            _clientes = database.GetCollection<Cliente>(settings.ClientesCollectionName);
+        }
+
+        public async Task<string> Create(Cliente cliente)
+        {
+            await _clientes.InsertOneAsync(cliente);
+            return cliente.Id;
+        }
+
+        public async Task<Cliente> Get(string id)
+        {
+            var cliente = await _clientes.FindAsync<Cliente>(cliente => cliente.Id == id);
+
+            if (cliente == null)
+                throw new OperationCanceledException($"Cliente não encontrado com o id {id}");
+
+            return cliente.First();
+        }
+
+        public async Task<List<Cliente>> GetAll()
+        {
+            var clientes = await _clientes.FindAsync<Cliente>(cliente => true);
+            return clientes?.ToList();
+        }   
+    }
+}
---
+++ Infra/Database/Repositories/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DesafioGamificacaoCPFL.Infra.Database.Repositories
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente> Get(string id);
+        Task<List<Cliente>> GetAll();
+        Task<string> Create(Cliente cliente);
+    }
+}
