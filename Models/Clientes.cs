6fe2bde35855676de4b4fd8e360ff4e7e3500dae
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-14 19:02:39 -0300

    add cliente id

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -1,9 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace DesafioGamificacaoCPFL
 {
     public class Cliente
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; internal set; }
+
         public string Nome{ get; internal set; }
 
         public string Cpf { get; internal set; }
