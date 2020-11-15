5fcc43ce8753a5a2e1a40cb30243bf1a95dffdde
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 15:37:49 -0300

    add xp na mensagem

--- Services/PontuacaoClienteService.cs
+++ Services/PontuacaoClienteService.cs
@@ -46,8 +46,9 @@ namespace DesafioGamificacaoCPFL.Services
 
             await AtualizarPontuacaoCliente(pontuacaoCliente);
 
-            _pontuacaoClienteResponse.Mensagem = $"{mensagemAtingiuProximoNivel}Pontuação atualizada com sucesso, sua nova pontuação é {pontuacaoCliente.QuantidadePontosAtual}, " +
-                $"você precisa somar {pontuacaoCliente.QuantidadePontosNecessariosParaAtingirProximoNivel} pontos para atingir o próximo nível e conseguir ainda mais descontos!";
+            _pontuacaoClienteResponse.Mensagem = $"{mensagemAtingiuProximoNivel}Pontuação atualizada com sucesso, sua nova pontuação é {pontuacaoCliente.QuantidadePontosAtual}" +
+                $" e {pontuacaoCliente.QuantidadeXP} de XP, você precisa somar {pontuacaoCliente.QuantidadePontosNecessariosParaAtingirProximoNivel} " +
+                $"pontos para atingir o próximo nível e conseguir ainda mais descontos!";
 
             return _pontuacaoClienteResponse;
         }
