3348afac02c6b57fefe7320504f8ee2a02af53ba
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 02:09:42 -0300

    add forma pagamento

--- Models/PagamentoParcial.cs
+++ Models/PagamentoParcial.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using Microsoft.AspNetCore.SignalR;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace DesafioGamificacaoCPFL.Models
@@ -14,13 +15,37 @@ namespace DesafioGamificacaoCPFL.Models
         public decimal ValorRestante { get; internal set; }
         public decimal QuantidadeParcelas { get; set; }
         public decimal ValorDaParcela { get; internal set; }
-    }
+        public MetodoPagamento MetodoPagamento { get; set; }
+    }    
 
     public class PagamentoParcialResponse
-    {
+    { 
         public decimal ValorRestante { get; internal set; }
         public decimal ValorDaParcela { get; internal set; }
         public decimal QuantidadeParcelas { get; set; }
         public string Mensagem { get; internal set; }
+    }
+
+    public class MetodoPagamento
+    {
+        public bool Fatura { get; set; }
+        public CartaoCredito CartaoCredito { get; set; }
+        public DadosBancarios DadosBancarios { get; set; }
+    }
+
+    public class CartaoCredito
+    {
+        public int NumeroCartao { get; set; }
+        public string DataVencimento { get; set; }
+        public int NumeroVerso { get; set; }
+        public string NomeNoCartao { get; set; }
+    }
+
+    public class DadosBancarios
+    {
+        public int CodigoBanco { get; set; }
+        public int AgenciaNumero { get; set; }
+        public int ContaNumero { get; set; }
+        public string ContaDigito { get; set; }
     }
 }
