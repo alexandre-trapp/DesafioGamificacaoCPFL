a65ca6f2581ad3af1623d2ca097a2e99b0733c48
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 01:24:31 -0300

    criado PagamentoParcialService, calculos de valor restante, valor da parcela e mensagem de retorno com sucesso

---
+++ Services/PagamentoParcialService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DesafioGamificacaoCPFL.Models;
+using DesafioGamificacaoCPFL.Infra.Database.Repositories;
+
+namespace DesafioGamificacaoCPFL.Services
+{
+    public static class PagamentoParcialService
+    {
+        public static async Task<PagamentoParcialResponse> CadastrarNovoPagamentoParcial(IPagamentoParcialRepository pagamentoParcialRepository,
+            IClienteRepository clienteRepository,
+            PagamentoParcial pagamentoParcial)
+        {
+            CalcularValorRestanteDaFatura(pagamentoParcial);
+
+            CalcularValorDaParcela(pagamentoParcial);
+
+            await pagamentoParcialRepository.CadastrarNovoPagamentoParcial(pagamentoParcial);
+
+            return new PagamentoParcialResponse
+            {
+                ValorRestante = pagamentoParcial.ValorRestante,
+                ValorDaParcela = pagamentoParcial.ValorDaParcela,
+                Mensagem = $"Pagamento parcial para o cliente {(await clienteRepository.Get(pagamentoParcial.ClienteId)).Nome} cadastrado com sucesso"
+            };
+        }
+
+        private static void CalcularValorRestanteDaFatura(PagamentoParcial pagamentoParcial) => 
+            pagamentoParcial.ValorRestante = pagamentoParcial.ValorTotalFatura - pagamentoParcial.ValorPagoParcialmente;
+
+        private static void CalcularValorDaParcela(PagamentoParcial pagamentoParcial) =>
+            pagamentoParcial.ValorDaParcela = pagamentoParcial.ValorRestante / pagamentoParcial.QuantidadeParcelas;
+    }
+}
